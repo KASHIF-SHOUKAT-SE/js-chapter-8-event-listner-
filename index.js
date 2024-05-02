98cfee6b2d65b848e2b9b6232af577186351e979
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-05-02 14:21:41 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// let x = function(e){
+//     alert("hello world")}
+// btn.addEventListener('click', x)
+let y =function(e){
+    console.log(e.target)
+    alert("hello world2")
+}
+btn.addEventListener('click', y)
+
+
+// let a = prompt("what is your favorite number")
+// if(a == "2"){
+//     btn.removeEventLintener('click',x)
+// }
+
+
